4569e94bb8a0da34a140cd9bfc39732b3d8e6d88
Tyler Kirumba <Tyler>
2023-03-13 22:44:12 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file!
+let companyName = 'Scuber';
+
+const companyName2 = 'Scuber';
+
+const mostProfitableNeighborhood = "Chelsea";
+
+let mostProfitableNeighborhood2 = "Chelsea";
+
+const companyCeo = "Susan Smith";
+
+let companyCeo2 = "Susan Smith";
